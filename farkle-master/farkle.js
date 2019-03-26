b7ff57d8073f3e03e155dffb5d6c794c75d0216b
Micoo98 <37417540+Micoo98@users.noreply.github.com>
2019-03-26 17:06:07 -0400

    Uses Class Player in addplayer and drawgame

---
+++ farkle-master/farkle.js
@@ -0,0 +1,41 @@
+class Farkle {
+    constructor(){
+        this._dieCount = 6;
+        this._dieSides = 6;
+        this.dice = [];
+
+        this.players = [];
+
+        for(var i = 1; i<=this._dieCount; i++){
+            this.dice[i-1] = new Die(this._dieSides);
+        }
+    }
+
+    addPlayer(playerName){
+        var newPlayer = new Player();
+        newPlayer.name = playerName;
+        this.players.push(newPlayer);
+
+        
+        
+    }
+
+    drawGame(){
+        document.querySelector('#board').innerHTML='';
+        
+        this.players.forEach((player)=>{
+            this.drawPlayer(player.name);
+        });
+        // loop over the players
+        // display them on the screen
+        // display their score along side their names
+        // draw some kind of a dice interface for viewing current dice status
+    }
+
+    drawPlayer(player){
+        var p = document.createElement('div');
+        p.innerText = player;
+        document.querySelector('#board').appendChild(p);
+    }
+    
+}
